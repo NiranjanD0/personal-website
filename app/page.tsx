0386967347f3628598d4371dae6b24fc8d4485c3
NiranjanD0 <niranjan4281@gmail.com>
2025-12-15 08:28:31 +0530

    ¯⁠\⁠_⁠(⁠ツ⁠)⁠_⁠/⁠¯

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen bg-black">
+      <div className="text-white opacity-50 font-bold text-s text-center">The developer is probably binge-watching something instead of coding 😴</div>
+    </div>
+  );
+}
