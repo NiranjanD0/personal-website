b2a8f2cbdabd6c581fcc74ca936448dec1186391
NiranjanD0 <niranjan4281@gmail.com>
2025-12-16 07:24:31 +0530

    added animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='min-h-screen min-w-screen mx-auto flex justify-center items-center bg-[url("/assets/images/noise.svg")] bg-black bg-no-repeat md:bg-fixed bg-center bg-cover'>
-      <section className="max-w-[70vw] text-2xl text-white">
-        <h3>Hello Visitor!!</h3></section>
+      <section className="max-w-[70vw] text-2xl text-white animate-fade-up">
+        <h3 className=' text-5xl font-extrabold'>Hello Visitor!!</h3></section>
     </div>
   )
 }
