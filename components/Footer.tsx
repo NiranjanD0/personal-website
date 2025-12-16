6215f5fc7e1623738e427500dfaddc4187e938a7
NiranjanD0 <niranjan4281@gmail.com>
2025-12-16 11:45:30 +0530

    idk, did some shit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,27 @@
 "use client"
 import React from 'react'
+import { Github, Linkedin, Mail } from 'lucide-react';
 
 const Footer = () => {
     return (
-        <div className='bg-white/1 md:h-[45vh] h-[60vh] my-5 rounded-t-4xl flex justify-center items-center flex-col border-t border-white/10'>
-            <div className='min-w-[80vw] md:h-[25vh] h-[45vh] flex flex-col md:flex-row justify-around items-center border rounded-4xl border-white/10 bg-black/90'>
-                <div className='md:w-1/3 flex justify-center items-center'>
-                    <span className='md: mr-2'>&copy;2025</span>
-                    <span>Niranjan Dangi</span>
+        <div className='bg-white/3 md:h-[45vh] h-[60vh] mt-5 rounded-t-4xl flex justify-center items-center flex-col border-t border-white/10'>
+            <div className='md:min-w-[50vw] md:h-[25vh] w-[70vw] h-[45vh] flex flex-col-reverse md:flex-row justify-around items-center border rounded-4xl border-white/10 bg-black/90'>
+                <div className='md:w-1/3 flex flex-col justify-center items-center md:h-full h-1/3 font-titan text-sm'>
+                    <div>
+                        <span className='md: mr-1 text-white/40'>&copy;2025</span>
+                        <span>Niranjan Dangi</span>
+                    </div>
                 </div>
-                <div className='h-full md:w-1/3 flex justify-center items-center'>
-                    <img src="/assets/images/logo.svg" alt="GAY" className='md:h-[80%] h-[50%]'/>
+                <div className='md:h-full h-1/3 md:w-1/3 flex justify-center items-center'>
+                    <img src="/assets/images/logo.svg" alt="logo" className='h-[80%]' />
                 </div>
-                <div className='md:w-1/3'>
-                    
+                <div className='md:w-1/3 md:h-full h-1/3 gap-5 flex items-center justify-center text-white/50'>
+                    <a target="_blank" href="https://github.com/NiranjanD0"><Github className='hover:text-white transition-colors' /></a>
+                    <a target="_blank" href="https://www.linkedin.com/in/niranjand0"><Linkedin className='hover:text-white transition-colors' /></a>
+                    <a target="_blank" href="mailto:niranjan@niranjan0.xyz"><Mail className='hover:text-white transition-colors' /></a>
                 </div>
             </div>
-            <div>NIRANJAN</div>
+            {/* <div>NIRANJAN</div> */}
         </div>
     )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,24 +9,27 @@ const Navbar = () => {
     };
 
     return (
-        <nav className='z-10 border-b border-white/10 rounded-b-3xl min-w-screen h-13 flex justify-between items-center fixed font-bold text-white/70 bg-black/50 backdrop-blur-2xl md:backdrop-blur-sm'>
-
-            <button
-                onClick={() => scrollTo("home")}
-                className="mx-16 hover:text-white transition-colors"
-            >/</button>
-            <div className="flex gap-5 mx-16 text-xs">
-                <button
-                    onClick={() => scrollTo("home")}
-                    className="hover:text-white transition-colors"
-                >Projects</button>
-                <button
-                    onClick={() => scrollTo("home")}
-                    className="hover:text-white transition-colors"
-                >Contact</button>
-            </div>
-
-        </nav>
+        <>
+            <nav className='z-10 border-b border-white/10 rounded-b-3xl w-screen h-13 flex justify-between items-center fixed font-bold text-white/70 bg-black/50 backdrop-blur-2xl md:backdrop-blur-sm'>
+                <div className='w-[30%] flex justify-center items-center'>
+                    <button
+                        onClick={() => scrollTo("home")}
+                        className="font-semibold mx-16 hover:text-white transition-colors"
+                    >/</button>
+                </div>
+                <div className="flex justify-center items-center gap-5 mx-16 w-[30%] text-xs">
+                    <button
+                        onClick={() => scrollTo("home")}
+                        className="hover:text-white transition-colors"
+                    >Projects</button>
+                    <button
+                        onClick={() => scrollTo("home")}
+                        className="hover:text-white transition-colors"
+                    >Contact</button>
+                </div>
+            </nav>
+            <div className='fixed z-10 bottom-10 right-10'>LOL</div>
+        </>
     )
 }
 
