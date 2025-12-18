31eb14707b11fb6b426ea05f77be06d65a1162e5
NiranjanD0 <niranjan4281@gmail.com>
2025-12-18 07:56:09 +0530

    new navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,22 +10,24 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className='z-10 border-b border-white/10 rounded-b-3xl w-screen h-13 flex justify-between items-center fixed font-bold text-white/70 bg-black/50 backdrop-blur-2xl md:backdrop-blur-sm'>
-                <div className='w-[30%] flex justify-center items-center'>
-                    <button
-                        onClick={() => scrollTo("home")}
-                        className="font-semibold mx-16 hover:text-white transition-colors"
-                    >/</button>
-                </div>
-                <div className="flex justify-center items-center gap-5 mx-16 w-[30%] text-xs">
-                    <button
-                        onClick={() => scrollTo("home")}
-                        className="hover:text-white transition-colors"
-                    >Projects</button>
-                    <button
-                        onClick={() => scrollTo("home")}
-                        className="hover:text-white transition-colors"
-                    >Contact</button>
+            <nav className='z-10 w-screen mt-2 h-13 fixed font-bold'>
+                <div className='border border-white/10 rounded-2xl md:w-[70vw] h-12 flex items-center justify-between m-auto text-white/70 bg-black/50 backdrop-blur-2xl md:backdrop-blur-sm'>
+                    <div className='flex justify-center items-center ml-10'>
+                        <button
+                            onClick={() => scrollTo("home")}
+                            className="font-semibold hover:text-white transition-colors"
+                        >/</button>
+                    </div>
+                    <div className="flex justify-center items-center gap-5 text-xs mr-10">
+                        <button
+                            onClick={() => scrollTo("home")}
+                            className="hover:text-white transition-colors"
+                        >Projects</button>
+                        <button
+                            onClick={() => scrollTo("home")}
+                            className="hover:text-white transition-colors"
+                        >Contact</button>
+                    </div>
                 </div>
             </nav>
             <div className='fixed z-10 bottom-10 right-10'>LOL</div>
